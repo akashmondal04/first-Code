2e389d877242359209989990a84ccb77864e2492
akashmondal04 <mondalakash525@gmail.com>
2024-03-05 23:17:17 +0530

    Enter the radius to calculate Circumference of circle

---
+++ funtionToCalculateCircumference.java
@@ -0,0 +1,18 @@
+package first_code;
+
+import java.util.Scanner;
+
+public class funtionToCalculateCircumference {
+   public static float circumference(float r){
+        float Pi= 3.14f;//must have "f" after the float value
+        float cir=2*Pi*r;
+        return cir;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter thr Radius: ");
+        float r= sc.nextFloat();
+        System.out.println("Circumference of the circle: "+circumference(r));
+    }  
+}
