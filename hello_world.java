0e113ff6c4824a5656be2209694a757d34cc62a0
akashmondal04 <mondalakash525@gmail.com>
2024-03-05 18:15:27 +0530

    Java hello World change from cpp file

---
+++ hello_world.java
@@ -0,0 +1,13 @@
+package first_code;
+
+/**
+ * hello_world
+ */
+public class hello_world {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        
+    }
+
+   
+}
