12aa278fe811c44be573972cc1cd8381c6c07026
akashmondal04 <mondalakash525@gmail.com>
2024-03-05 17:50:56 +0530

    New Sum Function Java File Added

---
+++ sumfunction.java
@@ -0,0 +1,29 @@
+package first_code;
+
+import java.util.Scanner;
+
+/**
+ * sumfunction
+ */
+
+public class sumfunction {
+
+    public static int sum(int i,int j){
+    int sum=i+j;
+    return sum;
+}    
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+        System.out.print("Enter first Number: ");
+        int a=sc.nextInt();
+        System.out.print("Enter second Number: ");
+        int b=sc.nextInt();
+        int sum1=sum(a, b);
+       
+        System.out.println("The Sum of two numbers is: "+sum1);
+        
+    
+    
+}
+    
+}
